024a780b49c8cf9309f7a8972e9ffbfd7b89ec70
Matt Auer <mattauer@umich.edu>
2021-09-25 22:16:47 -0400

    refactor: lint code

--- test/testEscrow.test.js
+++ test/testEscrow.test.js
@@ -1,19 +1,19 @@
-const EscrowFactory = artifacts.require("EscrowFactory");
-const Escrow = artifacts.require("Escrow");
+const EscrowFactory = artifacts.require('EscrowFactory')
+const Escrow = artifacts.require('Escrow')
 
-contract("Escrow", async (accounts) => {
-    let escrowFactory;
-    let escrow;
+contract('Escrow', async (accounts) => {
+  let escrowFactory
+  let escrow
 
-    beforeEach(async () => {
-        escrowFactory = await EscrowFactory.deployed();
-        // buyer: accounts[0], escrow: accounts[1], seller: accounts[2]
-        await escrowFactory.createEscrow(accounts[1], accounts[2], { from: accounts[0] });
-        const escrowAddress = await escrowFactory.getDeployedEscrowContract(0);
-        escrow = await Escrow.at(escrowAddress);
-    });
+  beforeEach(async () => {
+    escrowFactory = await EscrowFactory.deployed()
+    // buyer: accounts[0], escrow: accounts[1], seller: accounts[2]
+    await escrowFactory.createEscrow(accounts[1], accounts[2], { from: accounts[0] })
+    const escrowAddress = await escrowFactory.getDeployedEscrowContract(0)
+    escrow = await Escrow.at(escrowAddress)
+  })
 
-    it("Deploys an Escrow", () => {
-        assert.ok(escrow.address);
-    });
-});
+  it('Deploys an Escrow', () => {
+    assert.ok(escrow.address)
+  })
+})
--- test/testEscrowFactory.test.js
+++ test/testEscrowFactory.test.js
@@ -1,23 +1,23 @@
-const EscrowFactory = artifacts.require("EscrowFactory");
+const EscrowFactory = artifacts.require('EscrowFactory')
 
-contract("EscrowFactory", async (accounts) => {
-    let escrowFactory;
+contract('EscrowFactory', async (accounts) => {
+  let escrowFactory
 
-    beforeEach(async () => {
-        escrowFactory = await EscrowFactory.deployed();
-    });
+  beforeEach(async () => {
+    escrowFactory = await EscrowFactory.deployed()
+  })
 
-    it("Deploys the EscrowFactory", () => {
-        assert.ok(escrowFactory.address);
-    });
+  it('Deploys the EscrowFactory', () => {
+    assert.ok(escrowFactory.address)
+  })
 
-    it("Can create and Get Escrow Contracts", async () => {
-        // buyer: accounts[0] (default account/msg sender) escrowAccount: accounts[1], seller: accounts[2]
-        await escrowFactory.createEscrow(accounts[1], accounts[2], { from: accounts[0] });
-        const deployedEscrowContract = await escrowFactory.getDeployedEscrowContract(0);
-        assert.ok(deployedEscrowContract);
-        await escrowFactory.createEscrow(accounts[1], accounts[2], { from: accounts[0] });
-        const deployedEscrowContracts = await escrowFactory.getDeployedEscrowContracts();
-        assert.lengthOf(deployedEscrowContracts, 2);
-    });
-});
+  it('Can create and Get Escrow Contracts', async () => {
+    // buyer: accounts[0] (default account/msg sender) escrowAccount: accounts[1], seller: accounts[2]
+    await escrowFactory.createEscrow(accounts[1], accounts[2], { from: accounts[0] })
+    const deployedEscrowContract = await escrowFactory.getDeployedEscrowContract(0)
+    assert.ok(deployedEscrowContract)
+    await escrowFactory.createEscrow(accounts[1], accounts[2], { from: accounts[0] })
+    const deployedEscrowContracts = await escrowFactory.getDeployedEscrowContracts()
+    assert.lengthOf(deployedEscrowContracts, 2)
+  })
+})
