beb0201a80d2958f9aa9dcfbcecc16ef7a566075
Matt Auer <mattauer@umich.edu>
2021-09-27 20:13:57 -0400

    test: escrow contract receives funds

--- test/testEscrow.test.js
+++ test/testEscrow.test.js
@@ -2,18 +2,26 @@ const EscrowFactory = artifacts.require('EscrowFactory')
 const Escrow = artifacts.require('Escrow')
 
 contract('Escrow', async (accounts) => {
-  let escrowFactory
-  let escrow
+  let escrowFactory;
+  let escrow;
+  let escrowAddress;
 
   beforeEach(async () => {
     escrowFactory = await EscrowFactory.deployed()
     // buyer: accounts[0], escrow: accounts[1], seller: accounts[2]
-    await escrowFactory.createEscrow(accounts[1], accounts[2], { from: accounts[0] })
-    const escrowAddress = await escrowFactory.getDeployedEscrowContract(0)
+    await escrowFactory.createEscrow(accounts[1], accounts[2], { from: accounts[0], value: web3.utils.toWei("2", 'ether') });
+    escrowAddress = await escrowFactory.getDeployedEscrowContract(0)
     escrow = await Escrow.at(escrowAddress)
   })
 
-  it('Deploys an Escrow', () => {
-    assert.ok(escrow.address)
+  it('Deploys an Escrow and sends funds', async () => {
+    assert.ok(escrow.address);
+    const balance = await web3.eth.getBalance(escrowAddress);
+    assert.strictEqual(web3.utils.fromWei(balance, 'ether'), '2');
   })
+
+  it('Send funds to contract', async () => {
+    await escrow.releaseFunds({ from: accounts[0] });
+    assert(accounts[0])
+  }); 
 })
