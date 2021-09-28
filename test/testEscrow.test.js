f3e1721f143639ea5498ad2d23cbfb4168ab70d6
Matt Auer <mattauer@umich.edu>
2021-09-27 20:44:39 -0400

    test: seller and escrow can refund funds

--- test/testEscrow.test.js
+++ test/testEscrow.test.js
@@ -36,6 +36,14 @@ contract('Escrow', async (accounts) => {
       assert(err);
     }
   });
+
+  it('Seller can refund funds', async () => {
+    refundFundshelper(escrow, accounts[2], accounts[0]);
+  });
+
+  it('Escrow contract can refund funds', async () => {
+    refundFundshelper(escrow, accounts[1], accounts[0]);
+  });
 });
 
 
@@ -51,4 +59,18 @@ async function releaseFundsHelper(escrow, account, sellerAccount) {
   await escrow.releaseFunds({ from: account });
   const etherBalance = web3.utils.fromWei(await web3.eth.getBalance(sellerAccount), 'ether');
   assert.strictEqual(etherBalance, "102", "Seller account did not receive ether");
+}
+
+/**
+ * Summary.
+ * account refunds funds from the escrow account back to the buyer account
+ * require the account to be either seller or escrowAccoutn
+ * @param {Object} escrow escrow contract 
+ * @param {address} account address that refunds the fudns in the escrow account
+ * @param {address} buyerAccount address that receives the funds in the escrow account
+ */
+async function refundFundshelper(escrow, account, buyerAccount) {
+  await escrow.refundFunds({ from: account });
+  const etherBalance = parseFloat(web3.utils.fromWei(await web3.eth.getBalance(buyerAccount), 'ether'));
+  assert.isTrue(etherBalance <= 102, "Buyer account did not receive ether");
 }
