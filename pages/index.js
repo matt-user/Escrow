730d7ddbc3faab571e24f71adffa521d0a8a5fb3
Matt Auer <mattauer@umich.edu>
2021-09-28 13:35:32 -0400

    feat: add create escrow account page

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,19 @@
 import React, { Component } from 'react';
+import Link from 'next/link';
+import Layout from '../components/Layout';
+import { Button } from 'semantic-ui-react';
 
 class EscrowIndex extends Component {
     render() {
         return (
-            <h3>Escrow Accounts</h3>
+            <Layout>
+                <div>
+                    <h3>Escrow Accounts</h3>
+                    <Link href="/escrowAccounts/new">
+                        <a><Button floated="right" content="Create an Escrow Contract" icon="add circle" primary /></a>
+                    </Link>
+                </div>
+            </Layout>
         );
     }
 }
