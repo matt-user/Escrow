9e4468019c1cd2de6cf6bd17b3fbe41a01a0d49a
Matt Auer <mattauer@umich.edu>
2021-09-29 20:55:46 -0400

    work on getting redux to work with router

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,5 @@
-const EscrowFactory = artifacts.require('EscrowFactory')
+const EscrowFactory = artifacts.require("EscrowFactory");
 
 module.exports = (deployer) => {
-  deployer.deploy(EscrowFactory)
-}
+  deployer.deploy(EscrowFactory);
+};
