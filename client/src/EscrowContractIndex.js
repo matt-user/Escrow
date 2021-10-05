9529463f0395cdd1d0f66b2238544b4a8198aed5
Matt Auer <mattauer@umich.edu>
2021-10-05 19:56:08 -0400

    feat: add page to show individual escrow contract

---
+++ client/src/EscrowContractIndex.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Layout from './Layout';
+
+class EscrowContractIndex extends Component {
+    render() {
+        return (
+            <Layout>
+                <h2>Escrow Contract</h2>
+            </Layout>
+        );
+    }
+}
+
+export default EscrowContractIndex;
