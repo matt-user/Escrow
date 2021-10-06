ab3966005dee7204fe3e21c197ebe8729cfa51f8
Matt Auer <mattauer@umich.edu>
2021-10-05 21:31:24 -0400

    feat: buyer can release funds

--- client/src/EscrowContractIndex.js
+++ client/src/EscrowContractIndex.js
@@ -1,12 +1,35 @@
 import React, { Component } from 'react';
+import Web3 from 'web3';
+import { Button } from 'semantic-ui-react';
 import Layout from './Layout';
+import Escrow from "./contracts/Escrow.json";
+
+const web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:7545'));
 
 class EscrowContractIndex extends Component {
+
+    onRelease = async () => {
+        const { drizzle, drizzleState } = this.props;
+        const escrow = new web3.eth.Contract(Escrow.abi, this.props.address);
+        await escrow.methods.releaseFunds().send({ from: drizzleState.accounts[0] });
+    }
+
+    onRefund = async () => {
+        const { drizzleState } = this.props;
+        const escrow = new web3.Contract(Escrow.abi, this.props.match.params.address);
+        await escrow.methods.refundFunds().send({ from: drizzleState.accounts[0] });
+    }
+
     render() {
         return (
-            <Layout>
+            <div>
                 <h2>Escrow Contract</h2>
-            </Layout>
+                <h3>{this.props.match}</h3>
+                <div>
+                    <Button color="green" basic onClick={this.onRelease}>Release Funds to Seller</Button>
+                    <Button color="red" basoc onClick={this.onRefund}>Refund Funds to Buyer</Button>
+                </div>
+            </div>
         );
     }
 }
